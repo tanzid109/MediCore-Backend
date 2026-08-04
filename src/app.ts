d05d248da1164f39450369f49f0966674c0bd7b7
tanzid109 <tanzid.unkhalil109@gmail.com>
2026-08-04 21:05:39 +0600

    auth section done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,40 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
-import path from "path";
+import httpStatus from "http-status";
+import { AuthRoutes } from "./module/auth/auth.route";
+import config from "./config";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
+import { notFound } from "./middleware/notFound";
+
 
 const app: Application = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  }),
+);
+
+// Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/auth", AuthRoutes);
+
+// Basic route
 app.get("/", async (req: Request, res: Response) => {
-    res.send("MediCore API is running!");
-//   res.sendFile(path.join(process.cwd(), "index.html"));
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to Medicore Health Care System Backend",
+  });
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import app from "./app";
-import { prisma } from "./lib/prisma"
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-async function main(){
-try {
-    await prisma.$connect()
-    console.log("prisma connected");
-    app.listen(5000,()=>{
-        console.log("Server is running on port 5000");
-    })
-} catch (error) {
-    console.error("Error connecting to prisma:", error);
-    prisma.$disconnect()
-    process.exit(1)
-}
-}
 
-main()
+const PORT = config.port;
+
+const main = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
+
+main();
